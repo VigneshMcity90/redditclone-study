d89c59b6afcd848c500597ebc671bdc654cf26e8
vignesh manigandan <vigneshmanigandan112@gmail.com>
2020-11-02 20:04:25 +0530

    init commit

---
+++ src/main/java/com/demo/reddit/clone/rediitclone/RediitcloneApplication.java
@@ -0,0 +1,13 @@
+package com.demo.reddit.clone.rediitclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RediitcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RediitcloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/reddit/clone/rediitclone/RediitcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.reddit.clone.rediitclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RediitcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
